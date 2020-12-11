9db08391eee1372bd79099a2c0c2c20805c69540
Janelle Johnson <janelleblevins@gmail.com>
2020-12-11 16:00:32 -0600

    init-madlibs

---
+++ app.py
@@ -0,0 +1,22 @@
+"Mad Libs Generator"
+
+loop = 1
+while (loop < 10):
+    noun = input("Choose a noun: ")
+    plural_noun = input("Choose a plural noun: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
+
+    print ("------------------------------------------")
+    print ("Be kind to your",noun,"- footed", p_noun)
+    print ("For a duck may be somebody's", noun2,",")
+    print ("Be kind to your",plural_noun,"in",place)
+    print ("Where the weather is always",adjective,".")
+    print ()
+    print ("You may think that is this the",noun3,",")
+    print ("Well it is.")
+    print ("------------------------------------------")
+
+    loop = loop + 1
